068a847ddc07c2f6a652ee0fd707127f7e4f7331
ARSLAN <arslanzaheer977@gmail.com>
2021-02-20 16:01:33 +0500

    add services and contact page and signup page

--- cheetah/urls.py
+++ cheetah/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from cheetah.views import (
-    DashboardView
+    SignUpView,
+    IndexView,
+    ServicesView
 )
 
 urlpatterns = [
-    path('', DashboardView.as_view(), name='dashboard')
+    path('', SignUpView.as_view(), name='SignUp'),
+    path('index', IndexView.as_view(), name='Index'),
+    path('services', ServicesView.as_view(), name='Services'),
 ]
--- cheetah/views.py
+++ cheetah/views.py
@@ -3,5 +3,15 @@ from django.contrib.auth.views import TemplateView
 # Create your views here.
 
 
-class DashboardView(TemplateView):
-    template_name = 'dashboard.html'
+class SignUpView(TemplateView):
+    template_name = 'signup.html'
+
+
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
+
+class ServicesView(TemplateView):
+    template_name = 'services.html'
+
+
